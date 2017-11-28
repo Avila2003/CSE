6239efdd20d644b32446f634b5b71b73905a4232
Avila2003 <34071879+Avila2003@users.noreply.github.com>
2017-11-28 10:46:35 -0800

    Added previous notes

--- Notes.py
+++ Notes.py
@@ -1,1 +1,15 @@
 # This is working
+
+print("Hello World")
+
+# Juan
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+print #  this makes a new line, in python 3.6, it looks like: print()
+print("see if you can figure this out")
+print(13 % 12)
